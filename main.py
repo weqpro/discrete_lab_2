76671fe1cc2931d18aef597771315876a313545b
weqpro <weqpro@gmail.com>
2024-12-12 17:18:09 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+def read_dot():
+    """
+    3 variants
+    """
+
+
+def dfs_recursive():
+    """
+    2 (3) variants
+    """
+
+
+def dfs_iterative():
+    """
+    1 variant
+    """
+
+
+def bfs_iterative():
+    """
+    1 variant
+    """
+
+
+def find_radius():
+    """
+    3 variants
+    """
